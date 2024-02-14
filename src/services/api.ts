a9c1cbddcaee0b632bd8a81aeb71b97021115c50
CatWeb <uzuntudor@yahoo.com>
2024-02-14 21:05:11 +0200

    rename argument

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import { gql } from "@apollo/client";
 
-export const getArticles = (items: number) => gql`
+export const getArticles = (additionalArticles: number) => gql`
   query articles {
     contents(
       project_id: "5107de83-f208-4ca4-87ed-9b69d58d16e1", 
       lang: "ru", 
-      skip: ${items}, 
+      skip: ${additionalArticles}, 
       take: 10
     ) {
       id,
